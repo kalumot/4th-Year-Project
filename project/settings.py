60d6d8aff43fd1ccdb22aa8f8eb9c0186ba30a37
kalumot <kalumotoole@gmail.com>
2019-04-12 22:22:34 +0100

    Add files via upload

--- project/settings.py
+++ project/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    #'ammamanager'
+    'ammamanager'
 ]
 
 ROOT_URLCONF = 'urls'
