7aa7ca95404fd52d9da6f5eb33b07e66884823ff
kalumot <kalumotoole@gmail.com>
2019-04-13 14:52:46 +0100

    Update promotions.py

--- ammamanager/views/promotions.py
+++ ammamanager/views/promotions.py
@@ -5,7 +5,7 @@ from django.db import transaction
 from django.db.models import Avg, Count
 from django.forms import inlineformset_factory
 from django.shortcuts import get_object_or_404, redirect, render
-from django.conf.urls import reverse, reverse_lazy
+from django.urls import reverse, reverse_lazy
 from django.utils.decorators import method_decorator
 from django.views.generic import (CreateView, DeleteView, DetailView, ListView,
                                   UpdateView)
