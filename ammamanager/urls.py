2c08000671e023d9da962ea43c8ba88ad972a2ad
kalumot <kalumotoole@gmail.com>
2019-04-13 20:21:15 +0100

    Update urls.py

--- ammamanager/urls.py
+++ ammamanager/urls.py
@@ -1,34 +1,27 @@
-from django.urls import include, path
+from django.conf.urls import *
 
 from .views import ammamanager, gyms, promotions
-
 urlpatterns = [
-    path('', ammamanager.home, name='home'),
-
-    path('gyms/', include(([
-        path('', gyms.GymHomeView.as_view(), name='gym_home'),
-        path('fighters/', gyms.ListFightersView.as_view(), name='fighter_list'),
-        path('fighters/<int:pk>', gyms.fighter_view, name='fighter'),
-        path('fighters/<int:pk>/<int:offer_pk>/accept', gyms.accept_fight, name='accept_fight'),
-        path('fighters/<int:pk>/delete', gyms.fighter_delete, name='fighter_delete'),
-        path('fighters/add/', gyms.FighterCreateView.as_view(), name='fighter_add'),
-    ], 'ammamanager'), namespace='gyms')),
-
-
-
-
-
-    path('promotions/', include(([
-        path('', promotions.PromotionHomeView.as_view(), name='promotion_home'),
-        path('events/', promotions.ListEventsView.as_view(), name='event_list'),
-        path('events/add/', promotions.EventCreateView.as_view(), name='event_add'),
-        path('events/<int:pk>/', promotions.EventView.as_view(), name='event'),
-        path('events/<int:pk>/finished', promotions.finished_event, name='finished_event'),
-        path('events/<int:pk>/bout/add', promotions.bout_add, name='bout_add'),
-        path('events/<int:pk>/bout/<int:bout_pk>/', promotions.BoutView, name='bout'),
-        path('events/<int:pk>/bout/<int:bout_pk>/finished/<int:fighter_pk>', promotions.finished_bout, name='finished_bout'),
-        path('events/<int:pk>/bout/<int:bout_pk>/offer/<int:fighter_pk>', promotions.offer, name='offer'),
-        path('events/<int:pk>/bout/<int:bout_pk>/removefighters', promotions.removeFighters, name='remove_fighters'),
-        path('events/<int:pk>/bout/<int:bout_pk>/offerfight', promotions.offer_fight, name='offer_fight'),
+    # Examples:
+    url(r'^gyms', include(([
+        url(r'^', gyms.GymHomeView.as_view(), name='gym_home'),
+        url(r'^fighters/', gyms.ListFightersView.as_view(), name='fighter_list'),
+        url(r'^fighters/<int:pk>', gyms.fighter_view, name='fighter'),
+        url(r'^fighters/<int:pk>/<int:offer_pk>/accept', gyms.accept_fight, name='accept_fight'),
+        url(r'^fighters/<int:pk>/delete', gyms.fighter_delete, name='fighter_delete'),
+        url(r'^fighters/add/', gyms.FighterCreateView.as_view(), name='fighter_add'),
+    ],'ammamanager'), namespace='gyms')),
+     url(r'^promotions/', include(([
+        url(r'^', promotions.PromotionHomeView.as_view(), name='promotion_home'),
+        url(r'^events/', promotions.ListEventsView.as_view(), name='event_list'),
+        url(r'^events/add/', promotions.EventCreateView.as_view(), name='event_add'),
+        url(r'^events/<int:pk>/', promotions.EventView.as_view(), name='event'),
+        url(r'^events/<int:pk>/finished', promotions.finished_event, name='finished_event'),
+        url(r'^events/<int:pk>/bout/add', promotions.bout_add, name='bout_add'),
+        url(r'^events/<int:pk>/bout/<int:bout_pk>/', promotions.BoutView, name='bout'),
+        url(r'^events/<int:pk>/bout/<int:bout_pk>/finished/<int:fighter_pk>', promotions.finished_bout, name='finished_bout'),
+        url(r'^events/<int:pk>/bout/<int:bout_pk>/offer/<int:fighter_pk>', promotions.offer, name='offer'),
+        url(r'^events/<int:pk>/bout/<int:bout_pk>/removefighters', promotions.removeFighters, name='remove_fighters'),
+        url(r'^events/<int:pk>/bout/<int:bout_pk>/offerfight', promotions.offer_fight, name='offer_fight'),
     ], 'ammamanager'), namespace='promotions')),
 ]
