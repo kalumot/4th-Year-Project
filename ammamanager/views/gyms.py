f0190198b4158fa062c13c48b13fa6250f96e530
kalumot <kalumotoole@gmail.com>
2019-04-13 14:43:42 +0100

    Update gyms.py

--- ammamanager/views/gyms.py
+++ ammamanager/views/gyms.py
@@ -4,7 +4,7 @@ from django.contrib.auth.decorators import login_required
 from django.db import transaction
 from django.db.models import Count
 from django.shortcuts import get_object_or_404, redirect, render
-from django.urls import reverse_lazy
+#from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, ListView, UpdateView
 
