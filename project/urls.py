c57c066459513e4667e2455763ff51f47bd44756
kalumot <kalumotoole@gmail.com>
2019-04-13 14:40:12 +0100

    Update urls.py

--- project/urls.py
+++ project/urls.py
@@ -1,13 +1,18 @@
-#project.urls
-
-from django.conf.urls import include, path
+from django.conf.urls import *
 
 from ammamanager.views import ammamanager, gyms, promotions
 
 urlpatterns = [
-    path('', include('ammamanager.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('accounts/signup/', ammamanager.SignUpView.as_view(), name='signup'),
-    path('accounts/signup/gym/', gyms.GymSignUpView.as_view(), name='gym_signup'),
-    path('accounts/signup/promotion/', promotions.PromotionSignUpView.as_view(), name='promotion_signup'),
+    # Examples:
+    url(r'^$', include('ammamanager.urls')),
+    url(r'^accounts', include('django.contrib.auth.urls')),
+    url(r'^accounts/signup/', ammamanager.SignUpView.as_view(), name='signup'),
+    url(r'^accounts/signup/gym/', gyms.GymSignUpView.as_view(), name='gym_signup'),
+    url(r'^accounts/signup/promotion/', promotions.PromotionSignUpView.as_view(), name='promotion_signup'),
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
 ]
+
