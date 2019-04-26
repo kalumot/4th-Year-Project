6aa71537028f1b9c6da561bf0abac91080bb615a
kalumot <kalumotoole@gmail.com>
2019-04-26 07:29:16 +0100

    Update urls.py

--- project/urls.py
+++ project/urls.py
@@ -8,6 +8,5 @@ urlpatterns = [
     path('accounts/signup/', ammamanager.SignUpView.as_view(), name='signup'),
     path('accounts/signup/gym/', gyms.GymSignUpView.as_view(), name='gym_signup'),
     path('accounts/signup/promotion/', promotions.PromotionSignUpView.as_view(), name='promotion_signup'),
-    path('static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
 
