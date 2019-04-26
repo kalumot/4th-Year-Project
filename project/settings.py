ffbd1c1838793580ea24f1b06004c41f61688252
kalumot <kalumotoole@gmail.com>
2019-04-26 07:56:01 +0100

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -112,7 +112,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT= os.path.join(BASE_DIR,'static-files')
+STATIC_ROOT= os.path.join(BASE_DIR,'static')
 
 STATICFILES_DIRS = [
     'D:/home/site/wwwroot/static',
