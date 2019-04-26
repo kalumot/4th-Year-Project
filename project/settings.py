d97856e206d83f78cd85f6c0df7ec513f3d0c726
kalumot <kalumotoole@gmail.com>
2019-04-26 23:05:30 +0100

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -110,7 +110,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-# STATIC_URL = 'https://kalumammamanager.scm.azurewebsites.net/wwwroot/static/'
+STATIC_URL = 'https://kalumammamanager.scm.azurewebsites.net/wwwroot/static/'
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATICFILES_DIRS = [
