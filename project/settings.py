234b3d670d191ddd4bd95fe3f10033a82795b351
kalumot <kalumotoole@gmail.com>
2019-04-26 18:56:16 +0100

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -109,8 +109,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-STATIC_ROOT = path.join(BASE_DIR, 'static').replace('\\','/')
+
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 # Custom Django auth settings
 
 AUTH_USER_MODEL = 'ammamanager.User'
